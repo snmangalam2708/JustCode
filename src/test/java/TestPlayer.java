826527bbdd9f8739fd6e1d424c22140cab142b1f
ruppal <raheeluppal323@gmail.com>
2020-03-13 18:53:54 -0400

    update

--- src/test/java/TestPlayer.java
+++ src/test/java/TestPlayer.java
@@ -1,2 +1,23 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.logging.Logger;
+
 public class TestPlayer {
+    private static final Logger LOGGER = Logger.getLogger(TestPlayer.class.getName());
+    @Test
+    public void playerDetailsTest(){
+        Player player = new Player("Raheel Uppal",Position.PG,6.3,230.5,32.6,8.6,9.2,Year.FM);
+        String expected = player.playerDetails(player);
+        String actual ="Name: Raheel Uppal\n" +
+                " Position: PG\n" +
+                " Height: 6.3\n" +
+                " Weight: 230.5\n" +
+                " PPG: 32.6\n" +
+                " APG: 8.6\n" +
+                " RPG 9.2\n" +
+                " Class: FM";
+        LOGGER.info(player.playerDetails(player));
+        Assert.assertEquals(expected, actual);
+    }
 }
--- src/test/java/TestRoster.java
+++ src/test/java/TestRoster.java
@@ -9,36 +9,44 @@ public class TestRoster {
 
     @Test
     public void sizeTest(){
-        Player player1 = new Player();
+        Player player1 = new Player("Raheel Uppal");
         Position pg = Position.PG;
-        Player player2 = new Player();
+        Player player2 = new Player("David Kelly");
         Position sg = Position.SG;
-        Player player3 = new Player();
+        Player player3 = new Player("Sandeep Manglam");
         Position c = Position.SF;
         Roster roster = new Roster();
         roster.addToRoster(player1,pg);
         roster.addToRoster(player2,sg);
-        roster.addToRoster(player3,pg);
+        roster.addToRoster(player3,c);
 
         Integer expected = roster.size();
         Integer actual = 3;
+
 
         Assert.assertEquals(expected, actual);
     }
 
     @Test
     public void listRosterTest(){
-        Player player = new Player("Raheel Uppal",Position.PG,6.3,230.5,32.6,8.6,9.2,Year.FM);
-        String expected = player.playerDetails(player);
-        String actual ="Name: Raheel Uppal\n" +
-                " Position: PG\n" +
-                " Height: 6.3\n" +
-                " Weight: 230.5\n" +
-                " PPG: 32.6\n" +
-                " APG: 8.6\n" +
-                " RPG 9.2\n" +
-                " Class: FM";
-        LOGGER.info(player.playerDetails(player));
-        Assert.assertEquals(expected, actual);
+        Player player1 = new Player("Raheel Uppal");
+        Position pg = Position.PG;
+        Player player2 = new Player("David Kelly");
+        Position sg = Position.SG;
+        Player player3 = new Player("Sandeep Manglam");
+        Position sf = Position.SF;
+        Player player4 = new Player("Mat Ascone");
+        Position c = Position.C;
+
+        Roster roster = new Roster();
+        roster.addToRoster(player1,pg);
+        roster.addToRoster(player2,sg);
+        roster.addToRoster(player3,sf);
+        roster.addToRoster(player4,c);
+
+        LOGGER.info(roster.listRoster(roster));
+
     }
+
+
 }
