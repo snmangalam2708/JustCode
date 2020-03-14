64822ff307df614eb4858f7ba7587129140d6be4
DavidKellyonGitHub <dave.kelly.purchase@gmail.com>
2020-03-14 16:45:59 -0400

    added parser class

--- src/main/java/TextParser.java
+++ src/main/java/TextParser.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
+import java.util.HashMap;
 
 public class TextParser {
+
+    public TextParser(){}
+
+    public HashMap<String, Integer> parseSeeds(){
+        return null;
+
+        //get logic from hamlet lab
+    }
+
 }
