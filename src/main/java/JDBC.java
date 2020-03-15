a7941ffa02171b9e8cbcad28368d94c61ca86477
DavidKellyonGitHub <dave.kelly.purchase@gmail.com>
2020-03-15 17:33:04 -0400

    added getTeamList method

--- src/main/java/JDBC.java
+++ src/main/java/JDBC.java
@@ -1,13 +1,11 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class JDBC {
     private final String url = "";
     private final String user = "";
     private final String password = "";
 
-    //connect to postGres database
+    //connect to postgres database
 
     public Connection connect(){
         Connection conn = null;
@@ -21,5 +19,18 @@ public class JDBC {
         return conn;
     }
 
+    public String getTeamList() {
+        String SQL = "SELECT * FROM school_name";
+        String teamList = "";
+        try (Connection conn = connect();
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery(SQL)) {
+            rs.next();
+            teamList = rs.getString("school_name");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
 
+        return teamList;
+    }
 }
